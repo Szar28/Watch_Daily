006fbdcb8695d861df5eafd0f5212d3485b9df6c
Matthew S <100392783+Szar28@users.noreply.github.com>
2026-09-18 23:13:39 -0400

    add function to add to database

--- src/constants/control_constants.h
+++ src/constants/control_constants.h
@@ -6,3 +6,6 @@
 namespace CONTROL_CONSTANTS {
     inline constexpr int ENTER_KEY = '\n';
 } // namespace CONTROL_CONSTANTS
+
+
+const int CHARACTER_LIMIT = 30;
--- src/database.cpp
+++ src/database.cpp
@@ -5,9 +5,13 @@ Database::Database() {};
 Database::~Database() {};
 
 void Database::Add_Watch(WINDOW* window) {
+
+    int code = 0;
+
     echo();
     werase(window);
     box(window, 0, 0);
+
     mvwprintw(window, 2, 3, "Enter a watch name: ");
     wrefresh(window);
     mvwgetstr(window, 2, 23, watchName);
@@ -15,4 +19,48 @@ void Database::Add_Watch(WINDOW* window) {
     mvwprintw(window, 3, 3, "The watch name: %s", watchName);
     wrefresh(window);
     noecho();
+
+    code = Add_To_Database(watchName);
+    if (code == 0) {
+        mvwprintw(window, 4, 3, "%s was entered successfully!", watchName);
+    } else {
+        mvwprintw(window, 4, 3, "%s was not successful.", watchName);
+    }
+    wrefresh(window);
+}
+
+int Database::Add_To_Database(char name[CHARACTER_LIMIT]) {
+    sqlite3* db = nullptr;
+    char* errMessage = nullptr;
+
+    int rc = sqlite3_open("watch.db", &db);
+    if (rc != SQLITE_OK) {
+        return rc;
+    }
+
+    const char* sqlCreate = "CREATE TABLE IF NOT EXISTS watches ("
+                            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            "name TEXT NOT NULL);";
+    
+    rc = sqlite3_exec(db, sqlCreate, nullptr, nullptr, &errMessage);
+    if (rc != SQLITE_OK) {
+        sqlite3_free(errMessage);
+    }
+
+    const char* sqlInsert = "INSERT INTO watches (name) VALUES (?);";
+    sqlite3_stmt* stmt = nullptr; 
+    
+    rc = sqlite3_prepare_v2(db, sqlInsert, -1, &stmt, nullptr);
+    if (rc != SQLITE_OK) {
+        sqlite3_free(errMessage);
+    }
+
+    std::string str(name);
+    sqlite3_bind_text(stmt, 1, str.c_str(), -1, SQLITE_TRANSIENT);
+
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_DONE) {
+        return -1;
+    }
+    return 0;
 }
